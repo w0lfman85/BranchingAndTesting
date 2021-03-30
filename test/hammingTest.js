976a9e81e7d924efed3e154916d1447a97c420a4
yuriibaryshev <yura.baryshev@gmail.com>
2021-03-30 12:26:05 +0300

    positive test cases were added

--- test/hammingTest.js
+++ test/hammingTest.js
@@ -16,11 +16,35 @@ describe('Hamming code testing', function() {
 
   it('encode correctly', function() {
     let dataToEncode = [0,0,0,0];
+    let expectedEncoded = [0,0,0,0,0,0,0];
     let encoded = hamming.encode(dataToEncode);
     assert.equal(encoded.length == 7, true);
     for(let i = 0; i < 7; i++) {
-      assert.equal(encoded[i], 0);
+      assert.equal(encoded[i], expectedEncoded[i]);
     }
-  
+
+    dataToEncode = [1,0,0,0];
+    expectedEncoded = [1,1,1,0,0,0,0];
+    encoded = hamming.encode(dataToEncode);
+    assert.equal(encoded.length == 7, true);
+    for(let i = 0; i < 7; i++) {
+      assert.equal(encoded[i], expectedEncoded[i]);
+    }
+
+    dataToEncode = [0,1,0,0];
+    expectedEncoded = [1,0,0,1,1,0,0];
+    encoded = hamming.encode(dataToEncode);
+    assert.equal(encoded.length == 7, true);
+    for(let i = 0; i < 7; i++) {
+      assert.equal(encoded[i], expectedEncoded[i]);
+    }
+
+    dataToEncode = [0,0,0,1];
+    expectedEncoded = [1,1,0,1,0,0,1];
+    encoded = hamming.encode(dataToEncode);
+    assert.equal(encoded.length == 7, true);
+    for(let i = 0; i < 7; i++) {
+      assert.equal(encoded[i], expectedEncoded[i]);
+    }
   })
 })
